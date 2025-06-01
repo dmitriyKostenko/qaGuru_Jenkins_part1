aab23d31778caa39099aa8b342576bba1ad7c533
kostenko.dv <dmitriy.kostenko@sbermarket.ru>
2025-06-01 12:21:45 +0300

    update configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,28 +7,25 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
 public class TestBase {
 
+    @BeforeEach
+    public void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @BeforeAll
     public static void setup() {
-
-        String selenoidHost = System.getProperty("selenoid_host", "selenoid.autotests.cloud");
-        String selenoidLogin = System.getProperty("selenoid_login", "user1");
-        String selenoidPassword = System.getProperty("selenoid_password", "1234");
-        String browser = System.getProperty("browser", "chrome");
-        String browserVersion = System.getProperty("browserVersion", "127.0");
-        String screenResolution = System.getProperty("screenResolution", "1920x1080");
 
         WebDriverManager.chromedriver()
                 .clearDriverCache()
                 .clearResolutionCache()
                 .setup();
-
-        SelenideLogger.addListener("allure", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -37,21 +34,21 @@ public class TestBase {
         ));
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = screenResolution;
-        Configuration.browser = browser;
-        Configuration.browserVersion = browserVersion;
+        Configuration.browserSize = System.getProperty("screenResolution", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");;
+        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
         Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
-                selenoidLogin,
-                selenoidPassword,
-                selenoidHost);
+                System.getProperty("selenoid_login", "user1"),
+                System.getProperty("selenoid_password", "1234"),
+                System.getProperty("selenoid_host", "selenoid.autotests.cloud"));
 
         Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
-    void addAttachments() {
+    public void addAttachments() {
         Attach.screenshotAs("Last screen");
         Attach.pageSource();
         Attach.browserConsoleLogs();
